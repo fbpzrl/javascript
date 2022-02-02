b65cf089eccbfb87f58517aa2fde14701efd303e
Fabio Piazzarollo <fbpzrl@gmail.com>
2022-02-02 20:16:52 -0300

    Tipos de dados

---
+++ Origamid/1 - JavaScript Iniciante/2 - tipos de dados/script.js
@@ -0,0 +1,14 @@
+var idade = 37;
+console.log(typeof idade);
+
+var nome = 'Fabio';
+var sobrenome = 'Bissoli';
+var nomeCompleto = nome + ' ' + sobrenome;
+console.log(nomeCompleto);
+
+console.log("It's time");
+
+//Template String
+var gols = 1000;
+var frase1 = `Romário fez ${gols} gols`; //acento crase
+console.log(frase1);
