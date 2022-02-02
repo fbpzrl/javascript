a1ed070724f3146284c9cd68cc9a4e65aa1d96e8
Fabio Piazzarollo <fbpzrl@gmail.com>
2022-02-02 19:43:09 -0300

    Variáveis

---
+++ Origamid/1 - JavaScript Iniciante/variaveis/script.js
@@ -0,0 +1,19 @@
+var nome = 'Fabio';
+var idade = 23;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = 'Bissoli',
+    cidade = 'Vitória';
+
+console.log(sobrenome, cidade);
+
+var precoAplicativo; //undefined
+console.log(precoAplicativo);
