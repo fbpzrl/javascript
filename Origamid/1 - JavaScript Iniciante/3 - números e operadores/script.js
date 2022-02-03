5f6e9533a08e7e04759621d08b817fa3d8dfb401
Fabio Piazzarollo <fbpzrl@gmail.com>
2022-02-03 15:05:04 -0300

    Números e Operadores

--- Origamid/1 - JavaScript Iniciante/3 - números e operadores/script.js
+++ Origamid/1 - JavaScript Iniciante/3 - números e operadores/script.js
@@ -9,3 +9,6 @@ console.log(multiplicacao);
 
 var divisao = 'Comprei 10' / 2;
 console.log(divisao); // NaN = Not a Number
+
+var operadorUnario = +'200' + 50; // colocar o + antes de uma String transforma em um número
+console.log(operadorUnario);
