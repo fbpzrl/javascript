a98f941a1a0c9b5ce3020c5885b5e6194789d734
Fabio Piazzarollo <fbpzrl@gmail.com>
2022-02-03 15:00:22 -0300

    Números e Operadores

---
+++ Origamid/1 - JavaScript Iniciante/3 - números e operadores/script.js
@@ -0,0 +1,11 @@
+var soma = '100' + 50;
+console.log(soma);
+
+var subtracao = '100' - 50;
+console.log(subtracao);
+
+var multiplicacao = '100' * '2';
+console.log(multiplicacao);
+
+var divisao = 'Comprei 10' / 2;
+console.log(divisao); // NaN = Not a Number
