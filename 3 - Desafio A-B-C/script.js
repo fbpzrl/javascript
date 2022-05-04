f6ff24c3828514e9e4dccb9060661e9f588dbe3b
Fabio Piazzarollo <fbpzrl@gmail.com>
2022-05-04 13:00:33 -0300

    Desafio 1: A-B-C

---
+++ 3 - Desafio A-B-C/script.js
@@ -0,0 +1,13 @@
+//Desafio A-B-C
+var a = 10;
+var b = 20;
+var c = null;
+
+document.write("A: " + a + "<br>B: " + b + "<br> C: " + c);
+
+//Desafio: inveter o valor de a e b
+c = a;
+a = b;
+b = c;
+
+document.write("<br><br>A: " + a + "<br>B: " + b + "<br> C: " + c);
