af5a03a15fb73ee2bf3e0f42b6a44bf0b083ef40
Fabio Piazzarollo <fbpzrl@gmail.com>
2022-05-04 08:02:17 -0300

    Concatenação

---
+++ 2 - Contatenação/script.js
@@ -0,0 +1,5 @@
+//contatenação
+var nome = prompt("Digite o seu nome:");
+var idade = prompt("Digite sua idade:");
+document.write("<h1>Olá, " + nome + ", tudo bem?</h1>"); //A Classe document permite interagir com o Body
+document.write("<h2>Vi que você tem "+ idade + " anos.");
--- meu_script.js
+++
@@ -1,3 +0,0 @@
-alert("Olá, seja bem-vindo ao curso!");
-
-document.getElementById('nome').value = 'Oi';
