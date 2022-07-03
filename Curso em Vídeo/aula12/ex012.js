23deb786f27300b66678b3bc3b3539cb340cb760
Fabio Piazzarollo <fbpzrl@gmail.com>
2022-07-03 20:37:51 -0300

    Criando diretório Origamid

---
+++ Curso em Vídeo/aula12/ex012.js
@@ -0,0 +1,16 @@
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas.`)
+
+if (hora < 12 && hora > 1) {
+    console.log('Bom dia!')
+}
+else if (hora >= 12 && hora <= 18) {
+    console.log('Boa tarde!')
+}
+else if (hora > 18 && hora <= 24) {
+    console.log('Boa noite!')
+}
+else {
+    console.log('Boa madrugada!')
+}
---
+++ Curso em Vídeo/aula12ex/ex014/scrpt.js
@@ -0,0 +1,25 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('img')
+    
+    var data = new Date()
+    var hora = data.getHours()
+    //hora = 20
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora < 12 && hora > 1) {
+        img.src = 'img/manha.jpg'
+        img.alt = 'Foto de uma manhã ensolarada'
+        document.body.style.background = '#e2cd9f'
+    }
+    else if (hora >= 12 && hora <= 18) {
+        img.src = 'img/tarde.jpg'
+        img.alt = 'Foto de um barco no mar a tarde'
+        document.body.style.background = '#b9846f'
+    }
+    else if (hora > 18 && hora <= 24) {
+        img.src = 'img/noite.jpg'
+        img.alt = 'Foto de uma noite de luar'
+        document.body.style.background = '#515154'
+    }
+}
