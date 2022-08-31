bf4045e1c84a466f64d66c98767ede8ac78f347b
Fabio Piazzarollo <fbpzrl@gmail.com>
2022-08-31 09:21:08 -0300

    DOM - O que é

---
+++ Origamid/9 - DOM/1 - O que é o DOM/script.js
@@ -0,0 +1,12 @@
+// Retorne o url da página atual utilizando o objeto window
+const urlAtual = window.location.href
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const classeAtivo = document.querySelector('.ativo')
+
+// Retorne a linguagem do navegador
+const linguagemNavegador = navigator.language
+
+// Retorne a largura da janela 
+const larguraJanela = window.innerWidth
