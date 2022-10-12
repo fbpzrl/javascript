0a813e8ea4a8b10baa9b6a4986fc660082bdb63b
Fabio Piazzarollo <fbpzrl@gmail.com>
2022-10-12 06:40:04 -0300

    Fetch API

---
+++ DevSuperior/nivelamento-javascript/17 - Fetch API/js/script.js
@@ -0,0 +1,27 @@
+/* FETCH
+*  É uma Promise.
+*/
+
+const cep = '01001000'
+
+const fetchResult = fetch(`https://viacep.com.br/ws/${cep}/json/`) //Por padrão, o Fetch faz um GET na URL informada
+
+fetchResult
+    .then(response => {
+        console.log("SUCESSO NO FETCH", response)
+        const json = response.json()
+        json
+            .then(result => {
+                console.log("SUCESSO NO JSON", result)
+            })
+            .catch(error => {
+                console.log("ERRO NO JSON", error)
+            })
+        
+    })
+    .catch(error => {
+        console.log("ERRO NO FETCH", error)
+})
+
+console.log("A")
+console.log("B")
