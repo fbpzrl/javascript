8927e3063b352096afb36f842e7268376d824862
Fabio Piazzarollo <fbpzrl@gmail.com>
2022-10-12 06:12:41 -0300

    Promises

---
+++ DevSuperior/nivelamento-javascript/16 - Promises/js/script.js
@@ -0,0 +1,30 @@
+/* PROMISE
+*  É um objeto que pode executar uma função Assíncrona e mudar o seu estado (PromiseState) 
+   de Pendente (pending) para Realizado (fulfilled) ou Rejeitado (rejected) depois de algum
+   tempo.
+   Promise é um modelo de execução muito utilizado para fazer operações Assíncronas (Requisições).
+*/
+
+function f(g, h) {
+    
+    setTimeout(() => { //setTimeout faz a função executar de forma Assíncrona
+        if (10 > 0) {
+            g("P")
+        }
+        else {
+            h("Q")
+        }
+    }, 3000)
+}
+
+const minhaPromise = new Promise(f)
+
+minhaPromise
+.then(valor => {
+    console.log("Realizou com o valor " + valor)
+})
+.catch(valor => {
+    console.log("Rejeitou com o valor " + valor)
+})
+
+console.log(minhaPromise)
