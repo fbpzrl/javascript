525e5de6b2e1175abd7a636f0c4cf754d901bd19
Fabio Piazzarollo <fbpzrl@gmail.com>
2023-03-24 16:53:24 -0300

    Data-attributes

--- Alura/robotron/js/main.js
+++ Alura/robotron/js/main.js
@@ -1,19 +1,19 @@
-const subtrair = document.querySelector('#subtrair')
-const somar = document.querySelector('#somar')
-const parte = document.querySelector('#braco')
+const controle = document.querySelectorAll('[data-controle]')
 
-const controle = document.querySelectorAll('.controle-ajuste')
 controle.forEach( (elemento) => {
     elemento.addEventListener('click', (evento) => {
-        manipulaDados(evento.target.textContent, parte)
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
     })
 })
 
-function manipulaDados(operacao, parte) {
+function manipulaDados(operacao, controle) {
+    
+    const peca = controle.querySelector('[data-contador]')
+
     if (operacao === '+') {
-        parte.value = parseInt(parte.value) + 1
+        peca.value = parseInt(peca.value) + 1
     }
-    else if (parseInt(parte.value) > 0) {
-        parte.value = parseInt(parte.value) - 1
+    else if (parseInt(peca.value) > 0) {
+        peca.value = parseInt(peca.value) - 1
     }
 }
