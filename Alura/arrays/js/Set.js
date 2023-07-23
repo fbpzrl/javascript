3723bcbf67f4226638e71d52318eda6b1cf733bf
Fabio Piazzarollo <fbpzrl@gmail.com>
2023-07-23 13:33:40 -0300

    Usando a classe Set

---
+++ Alura/arrays/js/Set.js
@@ -0,0 +1,7 @@
+const nomes = ["Ana", "Clara", "Maria", "Maria", "João", "João", "João"]
+
+const meuSet = new Set(nomes) //Set remove elementos duplicados de um Array
+
+const nomesAtualizados = [...meuSet]
+
+console.log(nomesAtualizados)
