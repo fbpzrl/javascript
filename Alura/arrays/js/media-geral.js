74d58015ef20234da16ce61bf93fee2523493bc1
Fabio Piazzarollo <fbpzrl@gmail.com>
2023-07-25 11:03:16 -0300

    Média Geral

--- Alura/arrays/js/media-geral.js
+++ Alura/arrays/js/media-geral.js
@@ -4,11 +4,15 @@ const salaPython = [7, 3.5, 8, 9.5]
 
 function calculaMedia(notasDaSala) {
 
-    const somaDasNotas = notasDaSala.reduce((acumulador, nota) => {
+    const somaDasNotas = notasDaSala.reduce((acumulador, nota) => { //O método Reduce itera (percorre) o Array somando todos os elementos. Ele recebe dois parâmetros: uma função callback e a posição inicial do Acumulador (no caso, 0).
         return acumulador + nota
     }, 0)
 
     return somaDasNotas / notasDaSala.length
 }
 
-calculaMedia(`A média da sala de JavaScript é ${calculaMedia(salaJS)}.`)
+calculaMedia(salaJS)
+console.log(`A média da sala de JavaScript é ${calculaMedia(salaJS)}.`)
+console.log(`A média da sala de Java é ${calculaMedia(salaJava)}.`)
+console.log(`A média da sala de Python é ${calculaMedia(salaPython)}.`)
+
