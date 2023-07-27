192f8b0965eff3b8bf23b6828fd8fefbb5c6626b
Fabio Piazzarollo <fbpzrl@gmail.com>
2023-07-27 13:34:42 -0300

    Objetos

---
+++ Alura/objetos/aula1/objeto.js
@@ -0,0 +1,10 @@
+const listaCPFs = ["111111", "222222", "333333"]
+
+const informacoesPessoa = ["nome", "Jose", "idade", 32, "CPF", "1111222333"]
+
+console.log(informacoesPessoa[1])
+
+const objetoPessoa = {
+    nome : "José",
+    idade : 32,
+}
